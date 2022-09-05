d7bca4cab308da2c7faa1b1236124d8ee8bc44e1
FaiqID <54805607+faiqxid@users.noreply.github.com>
2022-09-05 23:11:45 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -71,6 +71,7 @@ echo -e "\e[1m\e[32m9. get host vnc  .... \e[0m" && sleep 1
 screen -dmS nvnn sh
 screen -S nvnn -X stuff "ngrok tcp 5901
 "
+
 echo -e "\e[1m\e[32m2. get host vnc  .... \e[0m" && sleep 1
 
 screen -r nvnn
